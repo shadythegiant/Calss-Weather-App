e33020e078cdb080f8e53054cc6697d636b434b3
shadythegiant <hellgatesdesigner2010@gmail.com>
2025-03-01 19:19:23 +0000

    initial commit :octocat:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { location: "lisbon" };
+    this.fetchWeather = this.fetchWeather.bind(this);
+  }
+  // Methods
+
+  fetchWeather() {
+    console.log("loading");
+    console.log(this);
+  }
+
+  //
   render() {
-    return <div className="">me</div>;
+    return (
+      <div className="app">
+        <h1>Classy Weather</h1>
+        <input
+          type="text"
+          placeholder="search for location"
+          value={this.state.location}
+          onChange={(e) => this.setState({ location: e.target.value })}
+        />
+        <button onClick={this.fetchWeather}>Get Weather</button>
+      </div>
+    );
   }
 }
 
